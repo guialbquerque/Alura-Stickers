1be6552707b54ecd890d8ae678906010876cd7db
guialbquerque <guialbquerque@gmail.com>
2022-07-20 01:42:50 -0300

    The class to generate stickers was created and now its possible print the images from url with a customized frase

---
+++ src/StickerGenerater.java
@@ -0,0 +1,42 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Font;
+
+public class StickerGenerater {
+    
+    public void create(InputStream inputStream, String nameFile) throws Exception{//Method to create stickers for whatsapp
+        //The images will be read, resize, write something below, apply transparency into the text and save them
+
+        //Readying image through an URL  
+        //InputStream inputStream = new URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_1.jpg").openStream(); 
+        
+        //1-Readying the images
+        BufferedImage originalImage = ImageIO.read(inputStream);
+
+        //2-Creating a new image in memory with transparency and new size
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+        int newHeight = height + 200;
+        BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
+
+        //3-Copy original images to new images (in memory)
+        Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+        graphics.drawImage(originalImage, 0, 0, null);
+
+        //Setup the font
+
+        Font font = new Font(Font.SANS_SERIF, Font.BOLD, 72);
+        graphics.setColor(Color.RED);
+        graphics.setFont(font);
+        //4-Write a phrase in each image
+        graphics.drawString("TOPZERA", 200, newHeight - 100);
+        //5-Write the new images in a file
+        ImageIO.write(newImage, "png", new File("/mnt/c/Users/guial/OneDrive/Documents/Alura-Cursos/Imersao-Java/Alura-Stickers/output/nameFile.png"));
+
+    }
+
+}
