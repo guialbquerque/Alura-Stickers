8fed9218eeaa7e54f7c4ef74807e12c6bcce282e
guialbquerque <guialbquerque@gmail.com>
2022-07-23 13:55:39 -0300

    Created the class content which has the url and title of the content

---
+++ src/Content.java
@@ -0,0 +1,22 @@
+public class Content {
+    
+
+    private String urlImage;
+    private String title;
+
+
+    public Content(String urlImage, String title){
+        this.urlImage = urlImage;
+        this.title = title;
+    }
+
+    public String getUrlImage(){
+        return urlImage;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+
+}
