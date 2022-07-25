c16a66575c8f6fcc0db8f067e666e5947f04a2a5
guialbquerque <guialbquerque@gmail.com>
2022-07-25 17:56:20 -0300

    PostMapping make the connection with postman, allowing to --Post-- json documents, adding in the mongoDB repository api

--- src/main/java/br/com/alura/Programme/Languages/LanguageController.java
+++ src/main/java/br/com/alura/Programme/Languages/LanguageController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +20,8 @@ public List<Language> getLanguages() {
     return languages;
     }
 
+@PostMapping("/languages")
+public Language registerLanguage(@RequestBody Language language) {
+    return repository.save(language);    
+    }
 }
