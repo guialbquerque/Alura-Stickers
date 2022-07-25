0980e97154b16cebdbb45124f7da484dd0e3dd94
guialbquerque <guialbquerque@gmail.com>
2022-07-25 14:57:15 -0300

    Created a class to represent the programme languages ordered by ranking, in this first moment running in localhost using springboot packages with maven

--- src/main/java/br/com/alura/Programme/Languages/LanguageController.java
+++ src/main/java/br/com/alura/Programme/Languages/LanguageController.java
@@ -1,13 +1,24 @@
 package br.com.alura.Programme.Languages;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguageController {
     
-    @GetMapping(value="/languages")
-    public String getLanguages(){
-        return "Hello, Java!";
+    private List<Language> languages = 
+        List.of(
+            new Language("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1 ),
+            new Language("Python", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/python/python_256x256.png", 2 ),
+            new Language( "JavaScripy", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_256x256.png", 3)
+        );
+
+
+@GetMapping("/languages")
+public List<Language> getLanguages() {
+    return languages;
     }
+
 }
